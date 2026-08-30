60b6b0584bd725ac020cbf5f3a3b121298367aaa
panomHSS <106316378+panomHSS@users.noreply.github.com>
2026-08-30 19:55:44 +0700

    feat: implement echo plugin

--- backend/app/plugins/echo.py
+++ backend/app/plugins/echo.py
@@ -5,3 +5,12 @@ from .base import Plugin
 
 class EchoPlugin(Plugin):
     """Simple plugin used to validate the plugin framework."""
+
+    def execute(
+        self,
+        context: PluginExecutionContext,
+        request: PluginRequest,
+    ) -> PluginResult:
+        return PluginResult(
+            content=request.content,
+        )
